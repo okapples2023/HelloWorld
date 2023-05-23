5847e19de4e39ae2dd50c2c0544907f121382136
okapples <okapples@outlook.com>
2023-05-23 23:20:18 +0800

    codecademy

---
+++ codecademy/Tree.java
@@ -0,0 +1,8 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    //We’re going to introduce ourselves and print a tree to the screen.
+    System.out.println(“Hey there, I'm Ariel and I'm learning to code in Java!”);
+    System.out.println("Ready to get my hands dirty!");
+  }
+}
